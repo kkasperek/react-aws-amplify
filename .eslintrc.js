e75992af1a6afcd30328ead8f0ddae43f0a37757
kkasperek <kt.kasperek@gmail.com>
2020-07-31 13:52:19 -0500

    add amplify and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es6': true
+	},
+	"extends": ["airbnb"],
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+	"rules": {
+		// "indent": [2, "tab"],
+		
+		"comma-dangle": 0,
+		"linebreak-style": 0,
+		"no-console": 0,
+		"react/jsx-curly-spacing": 0,
+		"react/jsx-filename-extension": 0
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/style.css';
+import Amplify from 'aws-amplify';
 import App from './App';
+import config from './aws-exports';
 
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
